0892ef3f2ce4de98c05ef4faee8dd35c2a354626
Julia Naomi <naomijub@users.noreply.github.com>
2024-09-16 20:50:52 -0400

    Adds trait map_err to facilitate dealing with Results (#6)

--- src/error.rs
+++ src/error.rs
@@ -163,33 +163,190 @@ where
     }
 }
 
-#[test]
-fn create_permanent_error() {
-    let e = Error::permanent("err");
-    assert_eq!(e, Error::Permanent("err"));
+pub trait MapErr<T, E> {
+    fn map_transient(self) -> Result<T, Error<E>>;
+    fn map_permanent(self) -> Result<T, Error<E>>;
 }
 
-#[test]
-fn create_transient_error() {
-    let e = Error::transient("err");
-    assert_eq!(
-        e,
-        Error::Transient {
-            err: "err",
-            retry_after: None
+impl<T, E> MapErr<T, E> for Result<T, E> {
+    #[inline]
+    fn map_transient(self) -> Result<T, Error<E>> {
+        match self {
+            Ok(t) => Ok(t),
+            Err(e) => Err(Error::transient(e)),
         }
-    );
+    }
+
+    #[inline]
+    fn map_permanent(self) -> Result<T, Error<E>> {
+        match self {
+            Ok(t) => Ok(t),
+            Err(e) => Err(Error::permanent(e)),
+        }
+    }
 }
 
-#[test]
-fn create_transient_error_with_retry_after() {
-    let retry_after = Duration::from_secs(42);
-    let e = Error::retry_after("err", retry_after);
-    assert_eq!(
-        e,
-        Error::Transient {
-            err: "err",
-            retry_after: Some(retry_after),
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::error::Error as StdError;
+    use std::fmt;
+
+    #[test]
+    fn create_permanent_error() {
+        let e = Error::permanent("err");
+        assert_eq!(e, Error::Permanent("err"));
+    }
+
+    #[test]
+    fn create_transient_error() {
+        let e = Error::transient("err");
+        assert_eq!(
+            e,
+            Error::Transient {
+                err: "err",
+                retry_after: None
+            }
+        );
+    }
+
+    #[test]
+    fn create_transient_error_with_retry_after() {
+        let retry_after = Duration::from_secs(42);
+        let e = Error::retry_after("err", retry_after);
+        assert_eq!(
+            e,
+            Error::Transient {
+                err: "err",
+                retry_after: Some(retry_after),
+            }
+        );
+    }
+
+    #[test]
+    fn map_transient_keeps_ok() {
+        let result: Result<i32, Error<()>> = Ok(42).map_transient();
+        assert_eq!(result, Ok(42));
+    }
+
+    #[test]
+    fn map_transient_maps_err() {
+        let result: Result<(), Error<&str>> = Err("err").map_transient();
+        assert_eq!(
+            result,
+            Err::<(), Error<&str>>(Error::Transient {
+                err: "err",
+                retry_after: None
+            })
+        );
+    }
+
+    #[test]
+    fn map_permanent_keeps_ok() {
+        let result: Result<i32, Error<()>> = Ok(42).map_permanent();
+        assert_eq!(result, Ok(42));
+    }
+
+    #[test]
+    fn map_permanent_maps_err() {
+        let result: Result<(), Error<&str>> = Err("err").map_permanent();
+        assert_eq!(result, Err(Error::Permanent("err")));
+    }
+
+    #[test]
+    fn fmt_permanent_error() {
+        let error = Error::Permanent("permanent error");
+        let formatted = format!("{}", error);
+        assert_eq!(formatted, "permanent error");
+    }
+
+    #[test]
+    fn fmt_transient_error() {
+        let error = Error::Transient {
+            err: "transient error",
+            retry_after: None,
+        };
+        let formatted = format!("{}", error);
+        assert_eq!(formatted, "transient error");
+    }
+
+    #[test]
+    fn debug_permanent_error() {
+        let error = Error::Permanent("permanent error");
+        let debug = format!("{:?}", error);
+        assert_eq!(debug, "Permanent(\"permanent error\")");
+    }
+
+    #[test]
+    fn debug_transient_error() {
+        let error = Error::Transient {
+            err: "transient error",
+            retry_after: None,
+        };
+        let debug = format!("{:?}", error);
+        assert_eq!(debug, "Transient(\"transient error\")");
+    }
+
+    #[test]
+    fn description_permanent_error() {
+        let error = Error::permanent(MyError("permanent error"));
+        assert_eq!(error.description(), "permanent error");
+    }
+
+    #[test]
+    fn description_transient_error() {
+        let error = Error::transient(MyError("transient error"));
+        assert_eq!(error.description(), "transient error");
+    }
+
+    #[test]
+    fn source_permanent_error() {
+        let error: Result<(), Error<MyError>> = Error::to_retry_after(
+            MyError("transient error"),
+            std::time::Duration::from_secs(1),
+        );
+        assert!(error.unwrap_err().source().is_none());
+    }
+
+    #[test]
+    fn source_transient_error() {
+        let error = Error::retry_after(
+            MyError("transient error"),
+            std::time::Duration::from_secs(1),
+        );
+        assert!(error.source().is_none());
+    }
+
+    #[test]
+    fn cause_permanent_error() {
+        let error = Error::permanent(MyError("permanent error"));
+        assert!(error.cause().is_none());
+    }
+
+    #[test]
+    fn cause_transient_error() {
+        let error = Error::transient(MyError("transient error"));
+        assert!(error.cause().is_none());
+    }
+
+    #[derive(Debug)]
+    pub struct MyError(pub &'static str);
+    impl fmt::Display for MyError {
+        fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+            write!(f, "{}", self.0)
         }
-    );
+    }
+    impl StdError for MyError {
+        fn description(&self) -> &str {
+            self.0
+        }
+
+        fn source(&self) -> Option<&(dyn StdError + 'static)> {
+            None
+        }
+
+        fn cause(&self) -> Option<&dyn StdError> {
+            self.source()
+        }
+    }
 }
